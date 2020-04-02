fd2cba9143b96f3bf76069cb982de96287edb73b
Gary Clark <gclark04432@live.co.uk>
2020-04-02 22:37:07 +0100

    add function to judge if correct answer selected

--- front-end/qui-generator-front-end/src/Components/QuestionComponents/Answer.js
+++ front-end/qui-generator-front-end/src/Components/QuestionComponents/Answer.js
@@ -6,24 +6,28 @@ function Answer({ currentQuestion }){
 
   if(!currentQuestion) return null;
 
+  const handleAnswerClicked = (e) => {
+    return e.target.textContent === currentQuestion.correct_answer ?
+      console.log("Correct answer clicked")
+      : console.log("Wrong Answer Clicked");
+    }
+
   const incorrectAnswers = currentQuestion.incorrect_answers.map((answer, index) => {
     return(
       <span
         className={"answer wrong-answer " + (revealed? 'answer-revealed' : null)}
-        key={index}>
+        key={index}
+        onClick={handleAnswerClicked}>
           {answer}
       </span>
     )
   })
 
-  const revealAnswer = () => {
-
-  }
-
   return (
     <div>
       <span
         className={"answer right-answer " + (revealed? 'answer-revealed' : null)}
+        onClick={handleAnswerClicked}
         >
           {currentQuestion.correct_answer}
       </span>
