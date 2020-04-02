c0aa0f82afbbf383acc49ad71703dcad69b7716a
Gary Clark <gclark04432@live.co.uk>
2020-04-02 10:25:57 +0100

    add-front-end-react-folders

---
+++ front-end/qui-generator-front-end/src/Containers/Main.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import './Main.css'
+
+function Main() {
+  return (
+    <main>
+      <div>
+        <p className="title-text">Hi, I'm the main container</p>
+      </div>
+    </main>
+  )
+}
+
+export default Main
