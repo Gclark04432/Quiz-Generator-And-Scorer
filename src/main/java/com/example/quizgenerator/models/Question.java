301eab8580af977d41d90d34212d9ae4e48a194e
Gary Clark <gclark04432@live.co.uk>
2020-04-02 10:08:22 +0100

    set up cors and add question components

---
+++ src/main/java/com/example/quizgenerator/models/Question.java
@@ -0,0 +1,52 @@
+package com.example.quizgenerator.models;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String question;
+
+    @Column
+    private List<String> answers;
+
+    public Question(){
+    }
+
+    public Question(String question, List<String> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+}
---
+++ src/main/java/com/example/quizgenerator/repositories/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.example.quizgenerator.repositories;
+
+import com.example.quizgenerator.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
