c6419595eac6c0b07d7010288c7b16c26d61f0d5
Gary Clark <gclark04432@live.co.uk>
2020-04-06 15:07:11 +0100

    add for each to loop all questions into new round

--- front-end/qui-generator-front-end/src/Containers/QuestionContainer/QuestionBox.js
+++ front-end/qui-generator-front-end/src/Containers/QuestionContainer/QuestionBox.js
@@ -28,59 +28,60 @@ function QuestionBox({ genre, difficulty, handleAnswerClicked }) {
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(roundNumber)
     })
-    .then(() => console.log(questionsToAdd[0]))
     .then(() => {
-      fetch("http://localhost:8080/questions", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(questionsToAdd[0])
+      questionsToAdd.forEach(question => {
+        fetch("http://localhost:8080/questions", {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(question)
+        })
       })
     })
-  // .then(() => fetch("http://localhost:8080/questions/round", {
-  //   method: 'POST',
-  //   headers: { "Content-Type": "text/uri-list" },
-  //    body: `http://localhost:8080/questions/1/${roundId}`
-  // }))
+    // .then(() => fetch("http://localhost:8080/questions/round", {
+    //   method: 'POST',
+    //   headers: { "Content-Type": "text/uri-list" },
+    //    body: `http://localhost:8080/questions/1/${roundId}`
+    // }))
 
 
-}
-
-const generateQuestions = () => {
-  fetch(`https://opentdb.com/api.php?amount=10&category=${genre}&difficulty=${difficulty}&type=multiple`)
-    .then(res => res.json())
-    .then(data => data.results)
-    .then(questions => setQuestions(questions))
-    .then(() => generateRound(roundId))
-    .then(() => console.log(questions))
   }
 
-  const customQuestions = () => {
-    fetch("http://localhost:8080/questions")
-    .then(res => res.json())
-    .then(data => data._embedded)
-    .then(questions => setQuestions(questions.questions))
-  }
-
-  const handleNextQuestionClicked = () => {
-    setQuestionIndex(questionIndex + 1);
-  }
-
-  const checkAnswerCorrect = (response, answersAllGiven) => {
-    handleAnswerClicked(response)
-    if (answersAllGiven) {
-      setTimeout(() => {
-        handleNextQuestionClicked();
-      }, 3000);
+  const generateQuestions = () => {
+    fetch(`https://opentdb.com/api.php?amount=10&category=${genre}&difficulty=${difficulty}&type=multiple`)
+      .then(res => res.json())
+      .then(data => data.results)
+      .then(questions => setQuestions(questions))
+      .then(() => generateRound(roundId))
+      .then(() => console.log(questions))
     }
+
+    const customQuestions = () => {
+      fetch("http://localhost:8080/questions")
+      .then(res => res.json())
+      .then(data => data._embedded)
+      .then(questions => setQuestions(questions.questions))
+    }
+
+    const handleNextQuestionClicked = () => {
+      setQuestionIndex(questionIndex + 1);
+    }
+
+    const checkAnswerCorrect = (response, answersAllGiven) => {
+      handleAnswerClicked(response)
+      if (answersAllGiven) {
+        setTimeout(() => {
+          handleNextQuestionClicked();
+        }, 3000);
+      }
+    }
+
+    return (
+      <section className="question-box">
+      <button onClick={customQuestions}>Click here for questions about your local area & interests</button>
+      <Question currentQuestion={questions[questionIndex]}/>
+      <Answer currentQuestion={questions[questionIndex]} handleAnswerClicked={checkAnswerCorrect} handleNextQuestionClicked={handleNextQuestionClicked}/>
+      </section>
+    )
   }
 
-  return (
-    <section className="question-box">
-    <button onClick={customQuestions}>Click here for questions about your local area & interests</button>
-    <Question currentQuestion={questions[questionIndex]}/>
-    <Answer currentQuestion={questions[questionIndex]} handleAnswerClicked={checkAnswerCorrect} handleNextQuestionClicked={handleNextQuestionClicked}/>
-    </section>
-  )
-}
-
-export default QuestionBox;
+  export default QuestionBox;
