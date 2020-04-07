53a196e8681d50efda6a1db359a861b5fb50f11c
Gary Clark <gclark04432@live.co.uk>
2020-04-07 14:44:36 +0100

    clicking begin round links to that rounds questions

--- front-end/qui-generator-front-end/src/Components/GameComponents/Round.js
+++ front-end/qui-generator-front-end/src/Components/GameComponents/Round.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Round.css';
 
 function Round({questions, roundNumber, getRoundQuestions}) {
 
   const handleBeginRoundClick = () => {
-    getRoundQuestions(roundNumber)
+    getRoundQuestions(roundNumber)  
   }
 
   return (
     <div className="round">
     <h1>Round {roundNumber}</h1>
-    <button onClick={handleBeginRoundClick}>Begin Round</button>
+    <Link to="/questions" onClick={handleBeginRoundClick}>Begin Round</Link>
     </div>
   )
 }
