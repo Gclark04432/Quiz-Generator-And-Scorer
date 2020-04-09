a9c587434ecdc620646c809be08b58e197f5921f
Gary Clark <gclark04432@live.co.uk>
2020-04-09 14:44:39 +0100

    add some css and new game link to home page

--- front-end/qui-generator-front-end/src/Components/HomePageComponents/HomePage.js
+++ front-end/qui-generator-front-end/src/Components/HomePageComponents/HomePage.js
@@ -1,10 +1,30 @@
 import  React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the home page</h1>
-    </div>
+    <article className="homepage">
+      <h1>Choose from thousands of questions on many categories!!!</h1>
+      <h4>Catgories range from Childrens cartoons to 18th Century Politics and include the below, plus many more...</h4>
+      <br/><br/><br/>
+      <span className="category-example">General Knowledge</span>
+      <span className="category-example">Cartoons & Animation</span>
+      <span className="category-example">Sports</span>
+      <span className="category-example">Music</span>
+      <span className="category-example">Film</span>
+
+      <br/><br/><br/>
+
+      <button className="new-game-button"><Link to="/new-game">Start New Game</Link></button>
+
+    </article>
   )
 }
 
